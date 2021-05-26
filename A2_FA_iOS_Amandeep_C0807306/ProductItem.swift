4368ed510d3d890c45c5cb748e5773104cf2566a
Amandeep03-sam <84222398+Amandeep03-sam@users.noreply.github.com>
2021-05-26 14:20:29 +0530

    Add files via upload
    final Exam

---
+++ A2_FA_iOS_Amandeep_C0807306/ProductItem.swift
@@ -0,0 +1,14 @@
+//
+//  ProductItem.swift
+//  A2_FA_iOS_Amandeep_C0807306
+//
+//  Created by Amandeep Kaur on 23/05/21.
+//
+
+import Foundation
+import CoreData
+
+@objc(ProductItem)
+public class ProductItem: NSManagedObject {
+
+}
---
+++ A2_FA_iOS_Amandeep_C0807306/ViewControllers/ProviderProductTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProviderProductTableViewCell.swift
+//  A2_FA_iOS_Amandeep_C0807306
+//
+//  Created by Amandeep Kaur on 25/05/21.
+//
+
+import UIKit
+
+class ProviderProductTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+  
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
